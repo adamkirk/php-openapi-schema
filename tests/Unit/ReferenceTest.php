31efd0aabe648a61371630e6fceae9e8ddacd195
Adam Kirk <adam_kirk@live.co.uk>
2025-06-29 13:06:22 +0100

    tests: add tests for reference and marhallingcontext

---
+++ tests/Unit/ReferenceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Spec;
+
+use OpenApiSchema\Reference;
+use PHPUnit\Framework\TestCase;
+use OpenApiSchema\Spec\Marshallable;
+use OpenApiSchema\Spec\MarshallingContext;
+
+/**
+ * @covers Reference
+ */
+class ReferenceTest extends TestCase
+{
+	public function test_initialization()
+	{
+		$ref = new Reference();
+
+		$this->assertNull($ref->getSummary());
+		$this->assertNull($ref->getDescription());
+		$this->assertNull($ref->getRef());
+
+		$this->assertInstanceOf(Marshallable::class, $ref);
+
+	}
+
+	public function test_getters_and_setters()
+	{
+		$ref = new Reference();
+
+		$ref->setSummary('summary');
+		$ref->setDescription('description');
+		$ref->setRef('ref');
+
+		$this->assertEquals('summary', $ref->getSummary());
+		$this->assertEquals('description', $ref->getDescription());
+		$this->assertEquals('ref', $ref->getRef());
+	}
+
+	public function test_its_marshallable()
+	{
+		$ref = new Reference();
+
+		$ref->setSummary('summary');
+		$ref->setDescription('description');
+		$ref->setRef('ref');
+
+		/** @var MockObject|MarshallingContext */
+		$ctx = $this->createMock(MarshallingContext::class);
+
+		$res = $ref->toMarshallable($ctx);
+
+		$this->assertEquals([
+			'description' => 'description',
+			'summary' => 'summary',
+			'$ref' => 'ref',
+		], $res);
+	}
+}
